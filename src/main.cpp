9732cf9f66ab1dad8f876157b01d24d19f697242
Fernanda <fecoimbra96@gmail.com>
2025-09-27 18:09:00 -0300

    feat: endpoint que retorna leitura da umidade

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,32 @@
 #include <WebServer.h>
 #include "secrets.h"  
 
-
-
 // definição do pino do sensor de umidade
 const int pinoSensorUmidade = 34;
-
 // Servidor HTTP na porta 80
 WebServer server(80);
+// Vetor para histórico de leituras
+#define MAX_HISTORICO 20
+int historico[MAX_HISTORICO];
+int indexHistorico = 0;
 
-// Função que retorna leitura atual no sensor de umidade de solo (4095 = seco, 0 = molhado)
+
+
+// Função que retorna leitura atual no sensor de umidade de solo (4095 = totalmente seco, 0 = molhado)
 int leituraUmidade()
 {
   return analogRead(pinoSensorUmidade);
+}
+
+void handleUmidade() {
+  int umidade = leituraUmidade();
+
+  // adiciona ao histórico de leituras
+  historico[indexHistorico] = umidade;
+  indexHistorico = (indexHistorico + 1) % MAX_HISTORICO;
+
+  String json = "{\"umidade\":" + String(umidade) + "}";
+  server.send(200, "application/json", json);
 }
 
 void setup()
@@ -36,6 +50,8 @@ void setup()
   Serial.print("Conectado! IP: ");
   Serial.println(WiFi.localIP());
 
+
+  server.on("/umidade", handleUmidade);
   server.begin();
   Serial.println("Servidor iniciado");
 }
